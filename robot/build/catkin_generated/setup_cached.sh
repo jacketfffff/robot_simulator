f20bd7fb19d3103af5b516705e1d5e2a8e5d9106
jacketfffff <1095117186@qq.com>
2020-10-31 15:29:26 +0800

    ssd

---
+++ robot/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/xcy/aworkspace/robot/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/xcy/aworkspace/robot/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/xcy/aworkspace/robot/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/xcy/aworkspace/robot/src:$ROS_PACKAGE_PATH"
